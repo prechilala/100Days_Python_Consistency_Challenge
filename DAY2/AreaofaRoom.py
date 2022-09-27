f97023f3c0bef30d98b0f43265a240335dbb8db1
prechilala <puzoma16@yahoo.com>
2022-09-27 06:58:01 -0700

    day2

---
+++ DAY2/AreaofaRoom.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+length = float(input("Enter the length of the room in feet : "))
+width = float(input("Enter the width of the room in feet : "))
+Area = length * width
+print('Area of the room = ', Area, 'square feet')
