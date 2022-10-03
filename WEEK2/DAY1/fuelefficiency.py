0467120564f4a3cc0616f5f69f906c33d523f6d2
prechilala <puzoma16@yahoo.com>
2022-10-03 15:44:03 -0700

    DAY6

---
+++ WEEK2/DAY1/fuelefficiency.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+# MPG - fuel efficiency for vehicles in united states(Miles per gallon)
+# L/100km - fuel efficiency for vehicles in canada (liters-per-hundred
+#kilometers)
+
+MPG = float(input("Enter the fuel efficiency in MPG: "))
+L = 235.215 / MPG
+print('Fuel efficiency in L/100km:', L)
