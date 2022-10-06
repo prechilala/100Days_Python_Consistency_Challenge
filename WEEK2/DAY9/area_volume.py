a6d062efaf405eee83929c64e6f5e33ab4f8e1b4
prechilala <puzoma16@yahoo.com>
2022-10-06 12:13:38 -0700

    DAY9

---
+++ WEEK2/DAY9/area_volume.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+from math import pi
+
+radius= r = int(input("Enter radius in metres: "))
+area_of_circle = Y = pi * (r ** 2)
+volume_of_sphere = X = (4 * pi * (r ** 3)) / 3
+
+print(f"The area of a circle = {Y:.2f} metres square")
+print(f"The volume of a sphere = {X:.2f} cubic metres")
