1a6505804eaea47937d0fe44b282959a07d658c8
prechilala <puzoma16@yahoo.com>
2022-10-05 17:02:46 -0700

    new task

---
+++ WEEK2/DAY8/distanceunit.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+feet = float(input('Enter the measurement of the distance in feet:'))
+inches = feet * 12
+yards = feet * 0.333333
+miles = feet * 0.000189394
+
+print(f'The equivalence of the distance entered in inches is: {inches}')
+print(f'The equivalence of the distance entered in yard is: {yards}')
+print(f'The equivalence of the distance entered in miles is: {miles}')
