8ff9f1fd5a61eb91b949d926f80094a013c5c992
prechilala <puzoma16@yahoo.com>
2022-10-10 14:51:24 -0700

    hotfix

--- WEEK3/DAY11/idealgaslaw.py
+++ WEEK3/DAY11/idealgaslaw.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 
-ideal gas constant = r = 8.314
+ideal_gas_constant = r = 8.314
 
 pressure = p = float(input("Enter pressure in Pascal: "))
 volume = v = float(input("Enter volume in liters: "))
 temperature = t = float(input("Enter temperature in degrees Celsius: "))
-
-temperature in kelvin = t += 273.15
-Amount of gas in moles= n = (p * v) / (r * t)
+#temperature in kelvin
+t += 273.15
+Amount_of_gas_in_moles = n = (p * v) / (r * t)
 
 print(f"The amount of gas is {n:.2f} moles.")
