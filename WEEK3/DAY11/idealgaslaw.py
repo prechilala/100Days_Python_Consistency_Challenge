a0d5349c22d95621d23df34e713604423c91c7bf
prechilala <puzoma16@yahoo.com>
2022-10-10 14:56:42 -0700

    hotfix

--- WEEK3/DAY11/idealgaslaw.py
+++ WEEK3/DAY11/idealgaslaw.py
@@ -5,8 +5,8 @@ ideal_gas_constant = r = 8.314
 pressure = p = float(input("Enter pressure in Pascal: "))
 volume = v = float(input("Enter volume in liters: "))
 temperature = t = float(input("Enter temperature in degrees Celsius: "))
-#temperature in kelvin
 
+#temperature in kelvin
 t += 273.15
 Amount_of_gas_in_moles = n = (p * v) / (r * t)
 
